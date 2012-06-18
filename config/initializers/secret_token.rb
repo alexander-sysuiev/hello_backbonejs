97818d1c9809fbae0a4520f97c6c754ed3f39ec1
alexander-sysuiev <aas@anahoret.com>
2012-06-18 14:37:20 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloBackbonejs::Application.config.secret_token = '5c3f35d9303123a648aeffec1e24e6f066c78992d2ab41bd87e2db1f58509604d855b2860fb401517ed4168ba4ce1ce96755ac4768f5dfc87a7604c746eb9650'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+HelloBackbonejs::Application.routes.draw do
+  root :to => "home#index"
+end
